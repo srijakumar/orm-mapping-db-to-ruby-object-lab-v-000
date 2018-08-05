f2f718e975219beafee4197b249a3f0e85c0616f
Srija S <srijakumar7@gmail.com>
2018-08-05 18:05:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
     sql = <<- SQL
-      select * 
+      select *
       FROM students
     SQL
 
